ef01a74d5976e3a57b978201f4aa3f42aeebba87
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 13:25:03 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,23 @@
+public class Pattern5 {
+public static void main(String[]args){
+    int n = 4;
+      for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                    System.out.print("*");
+            }
+            System.out.println();
+        }     
+        for(int i=n-1;i>=1;i--){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                    System.out.print("*");   
+            } 
+            System.out.println();   
+        }
+}
+}
